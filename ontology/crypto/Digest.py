b601cbfaf008125b06f5e42756f233161e8b49ad
wdx7266 <wdx7266@users.noreply.github.com>
2018-07-24 10:20:39 +0800

    Update Digest.py

--- ontology/crypto/Digest.py
+++ ontology/crypto/Digest.py
@@ -24,17 +24,17 @@ class Digest(object):
             return h.digest()
 
     @staticmethod
-    def sha256(byte_msg, offset=0, length=0, is_hex=False):
-        if offset != 0 and len(byte_msg) > offset + length:
-            byte_msg = byte_msg[offset:offset + length]
-        return Digest.__sha256(byte_msg, is_hex)
+    def sha256(msg: bytes, offset=0, length=0, is_hex=False):
+        if offset != 0 and len(msg) > offset + length:
+            msg = msg[offset:offset + length]
+        return Digest.__sha256(msg, is_hex)
 
     @staticmethod
-    def hash256(byte_msg, is_hex=False):
-        digest = Digest.sha256(Digest.sha256(byte_msg), is_hex)
+    def hash256(msg: bytes, is_hex=False):
+        digest = Digest.sha256(Digest.sha256(msg), is_hex)
         return digest
 
     @staticmethod
-    def hash160(byte_msg, is_hex=False):
-        digest = Digest.ripemd160(Digest.__sha256(byte_msg), is_hex)
+    def hash160(msg: bytes, is_hex=False):
+        digest = Digest.ripemd160(Digest.__sha256(msg), is_hex)
         return digest
