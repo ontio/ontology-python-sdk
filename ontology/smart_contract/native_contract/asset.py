f7ed291ae00bc99591b6caecd12fa3c991806bd2
lucas7788 <sishsh@163.com>
2018-07-27 17:22:11 +0800

    update asset.py

--- ontology/smart_contract/native_contract/asset.py
+++ ontology/smart_contract/native_contract/asset.py
@@ -9,6 +9,7 @@ from ontology.vm.params_builder import ParamsBuilder
 from ontology.vm.op_code import *
 from ontology.utils import util
 
+
 def new_transfer_transaction( asset:str, from_addr :str, to_addr :str, amount :int, gas_limit:int,gas_price:int):
     contract_address = get_asset_address(asset)  # []bytes
     state = [{"from": Address.decodeBase58(from_addr).to_array(), "to": Address.decodeBase58(to_addr).to_array(), "amount": amount}]
@@ -18,11 +19,12 @@ def new_transfer_transaction( asset:str, from_addr :str, to_addr :str, amount :i
     return Transaction(0, 0xd1, unix_timenow, gas_price, gas_limit, payer, invoke_code, bytearray(),
                        [], bytearray())
 
+
 def new_get_balance_transaction(asset, addr:str):
     contract_address = get_asset_address(asset)
     invoke_code = build_native_invoke_code(contract_address, bytes([0]), "balanceOf", Address.decodeBase58(addr).to_array())
     unix_timenow = int(time())
-    payer = Address("0000000000000000000000000000000000000000").to_array()
+    payer = Address(a2b_hex("0000000000000000000000000000000000000000".encode())).to_array()
     return Transaction(0, 0xd1, unix_timenow, 0, 0, payer, invoke_code, bytearray(),
                        [], bytearray())
 
@@ -32,7 +34,7 @@ def new_get_allowance_transaction(asset, from_addr:str, to_addr:str):
     args = {"from": Address.decodeBase58(from_addr).to_array(), "to": Address.decodeBase58(to_addr).to_array()}
     invoke_code = build_native_invoke_code(contract_address, bytes([0]), "allowance", args)
     unix_timenow = int(time())
-    payer = Address("0000000000000000000000000000000000000000").to_array()
+    payer = Address(a2b_hex("0000000000000000000000000000000000000000".encode())).to_array()
     return Transaction(0, 0xd1, unix_timenow, 0, 0, payer, invoke_code, bytearray(), [], bytearray())
 
 
@@ -44,7 +46,7 @@ def new_get_name_transaction(asset: str):
     contract_address = get_asset_address(asset)
     invoke_code = build_native_invoke_code(contract_address, bytes([0]), "name", bytearray())
     unix_timenow = int(time())
-    payer = Address("0000000000000000000000000000000000000000").to_array()
+    payer = Address(a2b_hex("0000000000000000000000000000000000000000".encode())).to_array()
     return Transaction(0, 0xd1, unix_timenow, 0, 0, payer, invoke_code, bytearray(), [], bytearray())
 
 
@@ -52,7 +54,7 @@ def new_get_symbol_transaction(asset: str):
     contract_address = get_asset_address(asset)
     invoke_code = build_native_invoke_code(contract_address, bytes([0]), "symbol", bytearray())
     unix_timenow = int(time())
-    payer = Address("0000000000000000000000000000000000000000").to_array()
+    payer = Address(a2b_hex("0000000000000000000000000000000000000000".encode())).to_array()
     return Transaction(0, 0xd1, unix_timenow, 0, 0, payer, invoke_code, bytearray(), [], bytearray())
 
 
@@ -60,7 +62,7 @@ def new_get_decimals_transaction(asset: str):
     contract_address = get_asset_address(asset)
     invoke_code = build_native_invoke_code(contract_address, bytes([0]), "decimals", bytearray())
     unix_timenow = int(time())
-    payer = Address("0000000000000000000000000000000000000000").to_array()
+    payer = Address(a2b_hex("0000000000000000000000000000000000000000".encode())).to_array()
     return Transaction(0, 0xd1, unix_timenow, 0, 0, payer, invoke_code, bytearray(), [], bytearray())
 
 
