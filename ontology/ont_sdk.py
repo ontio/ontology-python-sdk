2bf33a0c047d75d4479dc747d92157c7dfa69228
Kris Zhao <messixaviinsta0303@163.com>
2018-07-31 12:04:36 +0800

    update

--- ontology/ont_sdk.py
+++ ontology/ont_sdk.py
@@ -3,13 +3,13 @@ from ontology.core.sig import Sig
 from ontology.core.transaction import Transaction
 from ontology.utils import util
 from ontology.wallet.wallet_manager import WalletManager
-from ontology.rpc import rpc
+from ontology.rpc.rpc import RpcClient
 from ontology.common import define as Common
 
 
 class OntologySdk(object):
     def __init__(self):
-        self.rpc_client = rpc.RpcClient()
+        self.rpc_client = RpcClient()
         self.wallet_manager = WalletManager()
 
     def sign_transaction(self, tx: Transaction, signer: Account):
@@ -30,7 +30,6 @@ class OntologySdk(object):
         tx.sigs.append(sig)
         return tx
 
-
     def open_or_create_wallet(self, wallet_file):
         if util.is_file_exist(wallet_file):
             return self.open_wallet(wallet_file)
@@ -39,7 +38,7 @@ class OntologySdk(object):
     def create_wallet(self, wallet_file):
         if util.is_file_exist(wallet_file):
             raise IsADirectoryError("wallet file has already exist")
-        return WalletManager().open_wallet(wallet_file)
+        return self.wallet_manager.open_wallet(wallet_file)
 
     def open_wallet(self, wallet_file):
-        return WalletManager().open_wallet(wallet_file)
+        return self.wallet_manager.open_wallet(wallet_file)
