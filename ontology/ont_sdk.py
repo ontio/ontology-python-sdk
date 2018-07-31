57e7edb0e463116ae6bcc416d2d7f7d775df3653
lucas7788 <sishsh@163.com>
2018-07-31 10:37:04 +0800

    add sdk-test

--- ontology/ont_sdk.py
+++ ontology/ont_sdk.py
@@ -1,11 +1,12 @@
 from ontology.utils import util
-from ontology.account import account
+from ontology.wallet.wallet_manager import WalletManager
 from ontology.rpc import rpc
 
 
 class OntologySdk(object):
     def __init__(self):
         self.rpc_client = rpc.RpcClient()
+        self.wallet_manager = WalletManager()
 
     def open_or_create_wallet(self, wallet_file):
         if util.is_file_exist(wallet_file):
@@ -15,7 +16,7 @@ class OntologySdk(object):
     def create_wallet(self, wallet_file):
         if util.is_file_exist(wallet_file):
             raise IsADirectoryError("wallet file has already exist")
-        return account.open(wallet_file)
+        return WalletManager().open_wallet(wallet_file)
 
     def open_wallet(self, wallet_file):
-        return account.open(wallet_file)
+        return WalletManager().open_wallet(wallet_file)
---
+++ ontology/test_ontologySdk.py
@@ -0,0 +1,17 @@
+from unittest import TestCase
+
+from ontology.ont_sdk import OntologySdk
+
+
+class TestOntologySdk(TestCase):
+    def test_open_or_create_wallet(self):
+        sdk = OntologySdk()
+        a = sdk.open_wallet("./wallet/test.json")
+        print(a)
+
+    def test_open_or_create_wallet(self):
+        sdk = OntologySdk()
+        a = sdk.open_or_create_wallet("./test.json")
+
+        print(a)
+
