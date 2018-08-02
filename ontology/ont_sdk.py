82149dc7a0800c405b881515692d4e525f713625
lucas7788 <sishsh@163.com>
2018-08-02 15:24:42 +0800

    fix singleinstance bug

--- ontology/ont_sdk.py
+++ ontology/ont_sdk.py
@@ -5,7 +5,6 @@ from ontology.core.sig import Sig
 from ontology.core.transaction import Transaction
 from ontology.crypto.signature_scheme import SignatureScheme
 from ontology.smart_contract.native_vm import NativeVm
-from ontology.utils import util
 from ontology.wallet.wallet_manager import WalletManager
 from ontology.rpc.rpc import RpcClient
 from ontology.common import define as Common
@@ -14,20 +13,40 @@ from ontology.core.program import ProgramBuilder
 
 class OntologySdk(object):
     _instance_lock = threading.Lock()
+    __instance = None
 
     def __init__(self):
         self.rpc = RpcClient()
         self._wallet_manager = WalletManager()
-        self._native_vm = NativeVm(OntologySdk.get_instance())
-        self.defaultSignScheme = SignatureScheme.SHA256WITHECDSA
+        self._native_vm = None
+        self.defaultSignScheme = SignatureScheme.SHA256withECDSA
 
     @staticmethod
     def get_instance():
-        if not hasattr(OntologySdk, "_instance"):
+        if OntologySdk.__instance is None:
             with OntologySdk._instance_lock:
-                if not hasattr(OntologySdk, "_instance"):
-                    OntologySdk._instance = OntologySdk()
-        return OntologySdk._instance
+                if OntologySdk.__instance is None:
+                    OntologySdk.__instance = OntologySdk()
+        return OntologySdk.__instance
+
+    def native_vm(self):
+        if self._native_vm is None:
+            self._native_vm = NativeVm(OntologySdk.get_instance())
+        return self._native_vm
+
+    def get_wallet_manager(self):
+        if self._wallet_manager is None:
+            self._wallet_manager = WalletManager()
+        return self._wallet_manager
+
+    def get_rpc(self):
+        if self._rpc is None:
+            self._rpc = RpcClient()
+        return self._rpc
+
+    def set_signaturescheme(self, scheme: SignatureScheme):
+        self.defaultSignScheme = scheme
+        self._wallet_manager.set_signature_scheme(scheme)
 
     def sign_transaction(self, tx: Transaction, signer: Account):
         tx_hash = tx.hash256()
