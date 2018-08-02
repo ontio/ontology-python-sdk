140fcdac48a8651660f8a4cda5b93337233f1a99
dingxin <wdx7266@vip.qq.com>
2018-08-02 18:35:33 +0800

    normalize test

--- ontology/test/test_ontology_sdk.py
+++ ontology/test/test_ontology_sdk.py
@@ -24,31 +24,29 @@ multi_addr = Address.address_from_multi_pubKeys(2, pubkeys)
 
 
 class TestOntologySdk(TestCase):
-    # def test_open_wallet(self):
-    #     a = sdk.open_wallet("./wallet/test.json")
-    #     print(a)
-
-    def test_bb(self):
-        print(sdk.rpc.get_balance(acc.get_address_base58()))
-        print(sdk.rpc.get_balance(multi_addr.to_base58()))
+    def test_open_wallet(self):
+        a = sdk.open_wallet("./wallet/test.json")
+        print(a)
 
     def test_add_multi_sign_transaction(self):
         print(multi_addr.to_base58())
-        # tx = sdk.native_vm.asset.new_transfer_transaction("ong",acc.get_address_base58() ,multi_addr.to_base58(), 100000000,
-        #                                                   acc.get_address_base58(), 20000, 500)
-        # sdk.add_sign_transaction(tx, acc)
+        tx = sdk.native_vm().asset().new_transfer_transaction("ong", acc.get_address_base58(), multi_addr.to_base58(),
+                                                              100000000,
+                                                              acc.get_address_base58(), 20000, 500)
+        sdk.add_sign_transaction(tx, acc)
 
-        tx = sdk.native_vm.asset.new_transfer_transaction("ont", multi_addr.to_base58(), acc2.get_address_base58(), 1,
-                                                          acc.get_address_base58(), 20000, 500)
+        tx = sdk.native_vm().asset().new_transfer_transaction("ont", multi_addr.to_base58(), acc2.get_address_base58(),
+                                                              1,
+                                                              acc.get_address_base58(), 20000, 500)
         sdk.sign_transaction(tx, acc)
         sdk.add_multi_sign_transaction(tx, 2, pubkeys, acc)
         sdk.add_multi_sign_transaction(tx, 2, pubkeys, acc2)
         sdk.rpc.send_raw_transaction(tx)
 
-    def test_aa(self):
-        pubkeys = [acc.get_public_key(), acc2.get_public_key(), acc3.get_public_key()]
+    def test_sort_public_key(self):
+        pub_keys = [acc.get_public_key(), acc2.get_public_key(), acc3.get_public_key()]
         p = ProgramBuilder()
-        a = p.sort_publickeys(pubkeys)
-        print(a[0].hex())
-        print(a[1].hex())
-        print(a[2].hex())
+        a = p.sort_publickeys(pub_keys)
+        self.assertEqual("03036c12be3726eb283d078dff481175e96224f0b0c632c7a37e10eb40fe6be889", a[0].hex())
+        self.assertEqual("020f9ce29ede5f0e271b67e61b2480dccc98c3aabad095c604ef9ab1d92a475c0a", a[1].hex())
+        self.assertEqual("035384561673e76c7e3003e705e4aa7aee67714c8b68d62dd1fb3221f48c5d3da0", a[2].hex())
