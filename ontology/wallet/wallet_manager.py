47aea1dd4e0e3121ff83e21397e2619541c8745b
dingxin <wdx7266@vip.qq.com>
2018-08-15 10:06:42 +0800

    fix bugs

--- ontology/wallet/wallet_manager.py
+++ ontology/wallet/wallet_manager.py
@@ -14,7 +14,7 @@ import uuid
 from binascii import a2b_hex
 from ontology.wallet.identity import Identity, did_ont
 from ontology.wallet.identity_info import IdentityInfo
-from ontology.utils.util import get_random_bytes
+from ontology.utils.util import get_random_bytes, get_random_str
 from collections import namedtuple
 
 
@@ -37,43 +37,44 @@ class WalletManager(object):
         return self.wallet_file
 
     def load(self):
-        f = open(self.wallet_path, "r")
-        r = json.load(f, object_hook=lambda d: namedtuple('X', d.keys())(*d.values()))
-        f.close()
-        scrypt = Scrypt(r.scrypt.n, r.scrypt.r, r.scrypt.p, r.scrypt.dk_len)
+        with open(self.wallet_path, "r") as f:
+            r = json.load(f, object_hook=lambda d: namedtuple('X', d.keys())(*d.values()))
+            # f.close()
+            scrypt = Scrypt(r.scrypt.n, r.scrypt.r, r.scrypt.p, r.scrypt.dk_len)
 
-        identities = []
-        for index in range(len(r.identities)):
-            control = [Control(id=r.identities[index].controls[0].id,
-                               algorithm=r.identities[index].controls[0].algorithm,
-                               param=r.identities[index].controls[0].parameters,
-                               key=r.identities[index].controls[0].key,
-                               address=r.identities[index].controls[0].address,
-                               salt=r.identities[index].controls[0].salt,
-                               enc_alg=r.identities[index].controls[0].enc_alg,
-                               hash_value=r.identities[index].controls[0].hash,
-                               public_key=r.identities[index].controls[0].publicKey)]
-            temp = Identity(r.identities[index].ontid, r.identities[index].label, r.identities[index].lock, control)
-            identities.append(temp)
+            identities = []
+            for index in range(len(r.identities)):
+                control = [Control(id=r.identities[index].controls[0].id,
+                                   algorithm=r.identities[index].controls[0].algorithm,
+                                   param=r.identities[index].controls[0].parameters,
+                                   key=r.identities[index].controls[0].key,
+                                   address=r.identities[index].controls[0].address,
+                                   salt=r.identities[index].controls[0].salt,
+                                   enc_alg=r.identities[index].controls[0].enc_alg,
+                                   hash_value=r.identities[index].controls[0].hash,
+                                   public_key=r.identities[index].controls[0].publicKey)]
+                temp = Identity(r.identities[index].ontid, r.identities[index].label, r.identities[index].lock, control)
+                identities.append(temp)
 
-        accounts = []
-        for index in range(len(r.accounts)):
-            temp = AccountData(label=r.accounts[index].label, public_key=r.accounts[index].publicKey,
-                               sign_scheme=r.accounts[index].signatureScheme, is_default=r.accounts[index].isDefault,
-                               lock=r.accounts[index].lock, address=r.accounts[index].address,
-                               algorithm=r.accounts[index].algorithm, param=r.accounts[index].parameters,
-                               key=r.accounts[index].key, enc_alg=r.accounts[index].enc_alg,
-                               salt=r.accounts[index].salt, hash_value=r.accounts[index].hash)
-            accounts.append(temp)
+            accounts = []
+            for index in range(len(r.accounts)):
+                temp = AccountData(label=r.accounts[index].label, public_key=r.accounts[index].publicKey,
+                                   sign_scheme=r.accounts[index].signatureScheme,
+                                   is_default=r.accounts[index].isDefault,
+                                   lock=r.accounts[index].lock, address=r.accounts[index].address,
+                                   algorithm=r.accounts[index].algorithm, param=r.accounts[index].parameters,
+                                   key=r.accounts[index].key, enc_alg=r.accounts[index].enc_alg,
+                                   salt=r.accounts[index].salt, hash_value=r.accounts[index].hash)
+                accounts.append(temp)
 
-        res = WalletData(r.name, r.version, r.createTime, r.defaultOntid, r.defaultAccountAddress, scrypt,
-                         identities, accounts)
-        return res
+            res = WalletData(r.name, r.version, r.createTime, r.defaultOntid, r.defaultAccountAddress, scrypt,
+                             identities, accounts)
+            return res
 
     def save(self):
-        f = open(self.wallet_path, "w")
-        json.dump(self.wallet_in_mem, f, default=lambda obj: obj.__dict__, indent=4)
-        f.close()
+        with open(self.wallet_path, "w") as f:
+            json.dump(self.wallet_in_mem, f, default=lambda obj: obj.__dict__, indent=4)
+        # f.close()
         # f = open(wallet_path, 'r+')
         # s = f.read()
         # while "enc_alg" in s:
@@ -112,7 +113,7 @@ class WalletManager(object):
 
     def create_identity(self, label: str, pwd: str):
         priv_key = get_random_bytes(32)
-        salt = get_random_bytes(16)
+        salt = get_random_str(16)
         return self.__create_identity(label, pwd, salt, priv_key)
 
     def __create_identity(self, label: str, pwd: str, salt: str, private_key: bytes):
@@ -127,14 +128,14 @@ class WalletManager(object):
         return self.wallet_in_mem.get_identity_by_ontid(info.ontid)
 
     def create_identity_from_prikey(self, label: str, pwd: str, private_key: bytes):
-        salt = get_random_bytes(16)
+        salt = get_random_str(16)
         identity = self.__create_identity(label, pwd, salt, private_key)
         private_key = None
         return identity
 
     def create_account(self, label: str, pwd: str) -> AccountData:
         priv_key = get_random_bytes(32)
-        salt = get_random_bytes(16)
+        salt = get_random_str(16)
         account = self.__create_account(label, pwd, salt, priv_key, True)
         return self.wallet_file.get_account_by_address(account.get_address_base58())
 
@@ -147,6 +148,7 @@ class WalletManager(object):
             raise ValueError("scheme type is error")
         # set key
         if pwd != None:
+            from Cryptodome.Util.py3compat import tobytes, bord, _copy_bytes
             acct.key = account.export_gcm_encrypted_private_key(pwd, salt, Scrypt().get_n())
             pwd = None
         else:
@@ -165,7 +167,7 @@ class WalletManager(object):
                 acct.isDefault = True
                 self.wallet_in_mem.defaultAccountAddress = acct.address
             acct.label = label
-            acct.salt = base64.b64encode(salt.encode())
+            acct.salt = base64.b64encode(salt.encode()).decode('ascii')
             acct.publicKey = account.serialize_public_key().hex()
             self.wallet_in_mem.accounts.append(acct)
         else:
@@ -178,13 +180,15 @@ class WalletManager(object):
             if len(self.wallet_in_mem.identities) == 0:
                 idt.isDefault = True
                 self.wallet_in_mem.defaultOntid = idt.ontid
-            ctl = Control(id="keys-1", key=acct.key, salt=base64.b64encode(salt), address=acct.address,
+            ctl = Control(id="keys-1", key=acct.key, salt=base64.b64encode(salt.encode()).decode('ascii'),
+                          address=acct.address,
                           public_key=account.serialize_public_key().hex())
             idt.controls.append(ctl)
             self.wallet_in_mem.identities.append(idt)
         return account
 
-    def import_account(self, label: str, encrypted_prikey: str, pwd: str, base58_addr: str, salt: str):
+    def import_account(self, label: str, encrypted_prikey: str, pwd: str, base58_addr: str, base64_salt: str):
+        salt = base64.b64decode(base64_salt.encode('ascii')).decode('latin-1')
         private_key = Account.get_gcm_decoded_private_key(encrypted_prikey, pwd, base58_addr, salt, Scrypt().get_n(),
                                                           self.scheme)
         info = self.create_account_info(label, pwd, salt, private_key)
@@ -201,10 +205,11 @@ class WalletManager(object):
         info.public_key = acct.serialize_public_key().hex()
         info.encrypted_prikey = acct.export_gcm_encrypted_private_key(pwd, salt, Scrypt().get_n())
         info.address_u160 = acct.get_address().to_array().hex()
+        info.salt = salt
         return info
 
     def create_account_from_prikey(self, label: str, pwd: str, private_key: bytes):
-        salt = get_random_bytes(16)
+        salt = get_random_str(16)
         info = self.create_account_info(label, pwd, salt, private_key)
         for index in range(len(self.wallet_in_mem.accounts)):
             if info.address_base58 == self.wallet_in_mem.accounts[index].address:
@@ -240,7 +245,7 @@ if __name__ == '__main__':
     wallet_path = 'test.json'
     w = WalletManager()
     w.open_wallet(wallet_path)
-    salt = get_random_bytes(16)
+    salt = get_random_str(16)
     # w.import_account("123", encrypted_key, '234', acct0.get_address_base58(), salt)
     if False:
         w.create_random_account("label", "1")
