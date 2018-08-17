65de48f9afd334ccfcb3fcb81f73864ba33e472b
dingxin <wdx7266@vip.qq.com>
2018-08-17 13:45:08 +0800

    fix incorrect parameter value marking

--- ontology/exception/exception.py
+++ ontology/exception/exception.py
@@ -3,8 +3,10 @@
 
 
 class SDKException(Exception):
-    pass
+    def __init__(self, error_code: dict):
+        super().__init__(error_code['error'], error_code['desc'])
 
 
 class SDKRuntimeException(RuntimeError):
-    pass
+    def __init__(self, error_code: dict):
+        super().__init__(error_code['error'], error_code['desc'])
