411fe746dc50706ad62adc7c05ee1a488e466b92
dingxin <wdx7266@vip.qq.com>
2018-08-24 17:38:19 +0800

    make code more clearly

--- ontology/smart_contract/neo_vm.py
+++ ontology/smart_contract/neo_vm.py
@@ -19,24 +19,26 @@ class NeoVm(object):
 
     def send_transaction(self, contract_address: bytearray, acct: Account, payer_acct: Account, gas_limit: int,
                          gas_price: int, func: AbiFunction, pre_exec: bool):
-        params = bytearray()
         if func is not None:
             params = BuildParams.serialize_abi_function(func)
+        else:
+            params = bytearray()
         if pre_exec:
             tx = NeoVm.make_invoke_transaction(bytearray(contract_address), bytearray(params), b'', 0, 0)
             if acct is not None:
                 self.__sdk.sign_transaction(tx, acct)
             return self.__sdk.rpc.send_raw_transaction_pre_exec(tx)
-        unix_time_now = int(time())
-        params.append(0x67)
-        for i in contract_address:
-            params.append(i)
-        tx = Transaction(0, 0xd1, unix_time_now, gas_price, gas_limit, payer_acct.get_address().to_array(),
-                         params, bytearray(), [], bytearray())
-        self.__sdk.sign_transaction(tx, acct)
-        if payer_acct is not None and acct.get_address_base58() != payer_acct.get_address_base58():
-            self.__sdk.add_sign_transaction(tx, payer_acct)
-        return self.__sdk.rpc.send_raw_transaction(tx)
+        else:
+            unix_time_now = int(time())
+            params.append(0x67)
+            for i in contract_address:
+                params.append(i)
+            tx = Transaction(0, 0xd1, unix_time_now, gas_price, gas_limit, payer_acct.get_address().to_array(),
+                             params, bytearray(), [], bytearray())
+            self.__sdk.sign_transaction(tx, acct)
+            if payer_acct is not None and acct.get_address_base58() != payer_acct.get_address_base58():
+                self.__sdk.add_sign_transaction(tx, payer_acct)
+            return self.__sdk.rpc.send_raw_transaction(tx)
 
     @staticmethod
     def make_deploy_transaction(code_str: str, need_storage: bool, name: str, code_version: str, author: str,
@@ -59,10 +61,10 @@ class NeoVm(object):
         return deploy_tx
 
     @staticmethod
-    def make_invoke_transaction(code_addr: bytearray, params: bytearray, payer: bytes,
-                                gas_limit: int, gas_price: int):
+    def make_invoke_transaction(code_address: bytearray, params: bytearray, payer: bytes, gas_limit: int,
+                                gas_price: int):
         params += bytearray([0x67])
-        params += code_addr
+        params += code_address
         invoke_tx = InvokeTransaction()
         invoke_tx.version = 0
         invoke_tx.sigs = bytearray()
