f57441cd752a0c87ee92afce886d363fdaf6899d
dingxin <wdx7266@vip.qq.com>
2018-09-13 15:13:10 +0800

    update unit test

--- test/test_asset.py
+++ test/test_asset.py
@@ -4,23 +4,63 @@
 import time
 import unittest
 
-from ontology.account.account import Account
 from ontology.ont_sdk import OntologySdk
+from ontology.account.account import Account
 from ontology.crypto.signature_scheme import SignatureScheme
 from ontology.smart_contract.native_contract.asset import Asset
+from ontology.utils import util
 
 rpc_address = "http://polaris3.ont.io:20336"
 sdk = OntologySdk()
 sdk.rpc.set_address(rpc_address)
-private_key = "523c5fcf74823831756f0bcb3634234f10b3beb1c05595058534577752ad2d9f"
+private_key1 = "523c5fcf74823831756f0bcb3634234f10b3beb1c05595058534577752ad2d9f"
 private_key2 = "75de8489fcb2dcaf2ef3cd607feffde18789de7da129b5e97c81e001793cb7cf"
 private_key3 = "1383ed1fe570b6673351f1a30a66b21204918ef8f673e864769fa2a653401114"
-acc = Account(private_key, SignatureScheme.SHA256withECDSA)
+acc = Account(private_key1, SignatureScheme.SHA256withECDSA)
 acc2 = Account(private_key2, SignatureScheme.SHA256withECDSA)
 acc3 = Account(private_key3, SignatureScheme.SHA256withECDSA)
 
 
 class TestAsset(unittest.TestCase):
+    def test_query_name(self):
+        asset = sdk.native_vm().asset()
+        token_name = asset.query_name('ont')
+        self.assertEqual('ONT Token', token_name)
+        token_symbol = 'ong'
+        token_name = asset.query_name('ong')
+        self.assertEqual('ONG Token', token_name)
+
+    def test_query_symbol(self):
+        asset = Asset(sdk)
+        token_symbol = asset.query_symbol('ont')
+        self.assertEqual('ONT', token_symbol)
+        token_symbol = asset.query_symbol('ong')
+        self.assertEqual('ONG', token_symbol)
+
+    def test_query_decimals(self):
+        asset = Asset(sdk)
+        decimals = asset.query_decimals('ong')
+        self.assertEqual(9, decimals)
+        decimals = asset.query_decimals('ont')
+        self.assertEqual(1, decimals)
+
+    def test_query_balance(self):
+        asset = Asset(sdk)
+        private_key = util.get_random_str(64)
+        acct = Account(private_key, SignatureScheme.SHA256withECDSA)
+        b58_address = acct.get_address_base58()
+        balance = asset.query_balance('ont', b58_address)
+        self.assertTrue(isinstance(balance, int))
+        self.assertGreaterEqual(balance, 0)
+        balance = asset.query_balance('ong', b58_address)
+        self.assertTrue(isinstance(balance, int))
+        self.assertGreaterEqual(balance, 0)
+
+    def test_query_allowance(self):
+        a = Asset(sdk)
+        res = a.query_allowance("ont", acc.get_address().b58encode(), acc2.get_address().b58encode())
+        self.assertEqual(res, '01')
+
     def test_new_transfer_transaction(self):
         balance_1 = sdk.rpc.get_balance(acc.get_address_base58())
         balance_2 = sdk.rpc.get_balance(acc2.get_address_base58())
@@ -76,49 +116,21 @@ class TestAsset(unittest.TestCase):
         self.assertEqual(int(old_ong_balance_1), int(new_ong_balance_1))
         self.assertEqual((int(old_ong_balance_2) - int(new_ong_balance_2)), gas)
 
-    def test_query_balance(self):
-        a = Asset(sdk)
-        res = a.query_balance("ont", acc.get_address_base58())
-        print(res)
-        self.assertTrue(isinstance(res, int))
-
     def test_send_transfer(self):
         print(sdk.native_vm().asset().query_balance("ont", acc.get_address_base58()))
         print(sdk.native_vm().asset().query_balance("ont", acc3.get_address_base58()))
-        txhash = sdk.native_vm().asset().send_transfer("ont",acc,acc3.get_address_base58(),100,acc,20000,500)
+        txhash = sdk.native_vm().asset().send_transfer("ont", acc, acc3.get_address_base58(), 100, acc, 20000, 500)
         time.sleep(6)
         print(sdk.rpc.get_smart_contract_event_by_tx_hash(txhash))
         print(sdk.native_vm().asset().query_balance("ont", acc.get_address_base58()))
         print(sdk.native_vm().asset().query_balance("ont", acc3.get_address_base58()))
-
-    def test_query_allowance(self):
-        a = Asset(sdk)
-        res = a.query_allowance("ont", acc.get_address().b58encode(), acc2.get_address().b58encode())
-        self.assertEqual(res, '01')
-
-    def test_query_name(self):
-        a = Asset(sdk)
-        res = a.query_name('ont')
-        self.assertEqual(res, 'ONT Token')
-
-    def test_query_symbol(self):
-        a = Asset(sdk)
-        res = a.query_symbol('ont')
-        self.assertEqual(res, 'ONT')
-
-    def test_query_decimals(self):
-        a = Asset(sdk)
-        res = a.query_decimals("ong")
-        self.assertEqual(res, '09')
-        res = a.query_decimals("ont")
-        self.assertEqual(res, '01')
 
     def test_send_withdraw_ong_transaction(self):
         a = Asset(sdk)
         res = a.send_withdraw_ong_transaction(acc, acc2.get_address_base58(), 1, acc3, 20000, 500)
         self.assertEqual(res, '01')
 
-    def test_send_approve2(self):
+    def test_send_approve(self):
         a = Asset(sdk)
         res = a.send_approve("ont", acc, acc2.get_address_base58(), 1, acc3, 20000, 500)
         self.assertEqual(len(res), 64)
