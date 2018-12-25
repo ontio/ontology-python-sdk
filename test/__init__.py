4ef0d14a48b379df23d4e5f2e49e7db67668aff6
dingxin <wdx7266@vip.qq.com>
2018-12-25 18:46:40 +0800

    update for Travis

--- test/__init__.py
+++ test/__init__.py
@@ -1,8 +1,11 @@
-from os import path
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from os import path, environ
 
 from ontology.ont_sdk import OntologySdk
 
-password = 'password'
+password = environ['SDK_TEST_PASSWORD']
 sdk = OntologySdk()
 wallet_path = path.join(path.dirname(__file__), 'test_wallet.json')
 wallet_manager = sdk.wallet_manager
