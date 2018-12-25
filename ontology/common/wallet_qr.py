76d9f9ed515f9cda6c3bb90d093e9f17b6c6f7b6
dingxin <wdx7266@vip.qq.com>
2018-12-25 14:04:10 +0800

    PEP8

--- ontology/common/wallet_qr.py
+++ ontology/common/wallet_qr.py
@@ -1,59 +1,49 @@
-import base64
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 import json
+import base64
 
 from ontology.account.account import Account
-from ontology.crypto.signature_scheme import SignatureScheme
-from ontology.wallet.account import AccountData
 from ontology.wallet.wallet import WalletData
 from ontology.wallet.identity import Identity
+from ontology.wallet.account import AccountData
+from ontology.crypto.signature_scheme import SignatureScheme
 
 
 class WalletQR(object):
-
-    def export_identity_qrcode(self, wallet_file_or_scrypt, identity: Identity):
+    @staticmethod
+    def export_identity_qr_code(wallet_file_or_scrypt, identity: Identity):
         control = identity.controls[0]
         address = identity.ont_id[8:]
 
         if type(wallet_file_or_scrypt) is WalletData:
-            scrypt = json.dumps(wallet_file_or_scrypt.scrypt,
-                                default=lambda obj: obj.__dict__, sort_keys=True, indent=4)
+            scrypt = json.dumps(wallet_file_or_scrypt.scrypt, default=lambda obj: obj.__dict__, sort_keys=True,
+                                indent=4)
         else:
-            scrypt = json.dumps(wallet_file_or_scrypt,
-                                default=lambda obj: obj.__dict__, sort_keys=True, indent=4)
+            scrypt = json.dumps(wallet_file_or_scrypt, default=lambda obj: obj.__dict__, sort_keys=True, indent=4)
+        data = dict(type='I', label=identity.label, key=control.key, parameters=control.parameters, algorithm='ECDSA',
+                    scrypt=scrypt, address=address, salt=control.salt)
+        return data
 
-        d = dict(type="I",
-                 label=identity.label,
-                 key=control.key,
-                 parameters=control.parameters,
-                 algorithm="ECDSA",
-                 scrypt=scrypt,
-                 address=address,
-                 salt=control.salt)
-        return d
-
-    def export_account_qrcode(self, wallet_file_or_scrypt, account: AccountData):
+    @staticmethod
+    def export_account_qr_code(wallet_file_or_scrypt, account: AccountData):
         if type(wallet_file_or_scrypt) is WalletData:
-            scrypt = json.dumps(wallet_file_or_scrypt.scrypt,
-                                default=lambda obj: obj.__dict__, sort_keys=True, indent=4)
+            scrypt = json.dumps(wallet_file_or_scrypt.scrypt, default=lambda obj: obj.__dict__, sort_keys=True,
+                                indent=4)
         else:
-            scrypt = json.dumps(wallet_file_or_scrypt,
-                                default=lambda obj: obj.__dict__, sort_keys=True, indent=4)
+            scrypt = json.dumps(wallet_file_or_scrypt, default=lambda obj: obj.__dict__, sort_keys=True, indent=4)
+        data = dict(type='I', label=account.label, key=account.key, parameters=account.parameters, algorithm="ECDSA",
+                    scrypt=scrypt, address=account.b58_address, salt=account.salt)
+        return data
 
-        d = dict(type="I",
-                 label=account.label,
-                 key=account.key,
-                 parameters=account.parameters,
-                 algorithm="ECDSA",
-                 scrypt=scrypt,
-                 address=account.address,
-                 salt=account.salt)
-        return d
-
-    def get_prikey_from_qrcode(self, qr_code: str, password: str):
-        d = json.loads(qr_code)
-        key = d["key"]
-        address = d["address"]
-        salt = d["salt"]
-        n = json.loads(d["scrypt"])["n"]
-        return Account.get_gcm_decoded_private_key(key, password, address, base64.b64decode(salt),
-                                                   int(n), SignatureScheme.SHA256withECDSA)
+    @staticmethod
+    def get_private_key_from_qr_code(qr_code: str, password: str):
+        data = json.loads(qr_code)
+        key = data['key']
+        address = data['address']
+        salt = data['salt']
+        n = json.loads(data['scrypt'])['n']
+        acct = Account.get_gcm_decoded_private_key(key, password, address, base64.b64decode(salt), int(n),
+                                                   SignatureScheme.SHA256withECDSA)
+        return acct
