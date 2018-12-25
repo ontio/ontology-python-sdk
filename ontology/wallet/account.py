6e78a084a93fc977dc336e1230be203fca62960d
dingxin <wdx7266@vip.qq.com>
2018-12-25 14:32:16 +0800

    add property

--- ontology/wallet/account.py
+++ ontology/wallet/account.py
@@ -6,12 +6,12 @@ from ontology.exception.exception import SDKException
 
 
 class AccountData(object):
-    def __init__(self, address: str = '', enc_alg: str = "aes-256-gcm", key: str = "", algorithm="ECDSA", salt="",
-                 param: dict = None, label: str = "", public_key: str = "", sign_scheme: str = "SHA256withECDSA",
-                 is_default: bool = True, lock: bool = False):
+    def __init__(self, b58_address: str = '', enc_alg: str = "aes-256-gcm", key: str = "", algorithm: str = 'ECDSA',
+                 salt="", param: dict = None, label: str = "", public_key: str = "",
+                 sig_scheme: str = 'SHA256withECDSA', is_default: bool = True, lock: bool = False):
         if param is None:
-            param = {"curve": "P-256"}
-        self.__b58_address = address
+            param = dict(curve='P-256')
+        self.__b58_address = b58_address
         self.__algorithm = algorithm
         self.__enc_alg = enc_alg
         self.__is_default = is_default
@@ -21,7 +21,7 @@ class AccountData(object):
         self.parameters = param
         self.__salt = salt
         self.__public_key = public_key
-        self.__signature_scheme = sign_scheme
+        self.__signature_scheme = sig_scheme
 
     def __iter__(self):
         data = dict()
@@ -62,7 +62,9 @@ class AccountData(object):
         return self.__enc_alg
 
     @enc_alg.setter
-    def enc_alg(self, enc_alg):
+    def enc_alg(self, enc_alg: str):
+        if not isinstance(enc_alg, str):
+            raise SDKException(ErrorCode.other_error('Invalid encryption algorithm.'))
         self.__enc_alg = enc_alg
 
     @property
@@ -71,6 +73,8 @@ class AccountData(object):
 
     @is_default.setter
     def is_default(self, is_default: bool):
+        if not isinstance(is_default, bool):
+            raise SDKException(ErrorCode.other_error('Invalid default account state.'))
         self.__is_default = is_default
 
     @property
@@ -96,7 +100,9 @@ class AccountData(object):
         return self.__lock
 
     @lock.setter
-    def lock(self, lock):
+    def lock(self, lock: bool):
+        if not isinstance(lock, bool):
+            raise SDKException(ErrorCode.other_error('Invalid lock state.'))
         self.__lock = lock
 
     @property
