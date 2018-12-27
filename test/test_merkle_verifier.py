a50563913a41e2d5efb2b42aafaec372eca5d139
dingxin <wdx7266@vip.qq.com>
2018-12-27 22:45:00 +0800

    update unit test

--- test/test_merkle_verifier.py
+++ test/test_merkle_verifier.py
@@ -20,27 +20,6 @@ class TestMerkleVerifier(unittest.TestCase):
         result = MerkleVerifier.validate_proof(proof, target_hash, merkle_root)
         print(result)
 
-    def test_vv(self):
-        import requests
-        import json
-
-        eos_main_net = 'https://mainnet.eos.dfuse.io'
-        header = dict(
-            Authorization='Bearer eyJhbGciOiJLTVNFUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1NDcwODg4MjYsImp0aSI6IjU3MDM1NjVmLTI1YjUtNDkzNC05NWVmLWFjNDIzZDk0ZTc3MiIsImlhdCI6MTU0NDQ5NjgyNiwiaXNzIjoiZGZ1c2UuaW8iLCJzdWIiOiJDaVFBNmNieWU4ZzAyVTloYmdpMThGUk1MOXhXcW4vdHljeFl5QUdIVEF0SEI3WEhYcUVTT3dBL0NMUnRzOHdWd05xN0swVkZpTjd0dnZXREFvNXhRZmg2a1BrVUo0bTg5UFZsYTVBYTVwTE9yK1dKWDdlNVpzVzZKam9Od2ZQa2FXY0kiLCJ0aWVyIjoiYmV0YS12MSIsInYiOjF9.5cEPmu-9pg8IDmmVseaFNagX_I9VvzyoowwQxLv_Ie28L3lsYdNMF1J-R1-teVvJU6eOpNUBJCLi3ZdusChowg')
-        method = 'search/transactions'
-        url = f'{eos_main_net}/v0/{method}'
-        payload = dict(start_block=34226134, block_count=2000, limit=100, sort='desc',
-        q = 'wizznetwork1:eosio.token action:transfer')
-
-        data = requests.get(url=url, params=json.dumps(payload), headers=header)
-        print(data.content.decode('utf-8'))
-
-
-def test_s(self):
-    # print(ErrorCode.invalid_par)
-    # print(ErrorCode.invalid_params)
-    ErrorCode.a()
-
 
 if __name__ == '__main__':
     unittest.main()
