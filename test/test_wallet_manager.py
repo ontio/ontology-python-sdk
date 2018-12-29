ea0227cf1709c6f466efb3ca02c0a5d103b18955
dingxin <wdx7266@vip.qq.com>
2018-12-29 17:25:19 +0800

    update unit test for wallet manager

--- test/test_wallet_manager.py
+++ test/test_wallet_manager.py
@@ -53,7 +53,7 @@ class TestWalletManager(unittest.TestCase):
     def test_wallet_data(self):
         wm = WalletManager()
         path = os.path.join(os.path.dirname(__file__), 'wallet.dat')
-        wm.create_wallet(path)
+        self.assertRaises(SDKException, wm.create_wallet, path)
         wm.open_wallet(path)
         self.assertTrue(isinstance(wm, WalletManager))
         self.assertEqual(wm.__dict__['scheme'], SignatureScheme.SHA256withECDSA)
