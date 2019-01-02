54516c36c4a5777798c4e67575aa7f78df6339dc
dingxin <wdx7266@vip.qq.com>
2019-01-02 21:08:37 +0800

    update claim

--- ontology/claim/claim.py
+++ ontology/claim/claim.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import json
 import base64
 
 from time import time
+from typing import List
 
 from ontology.claim.header import Header
 from ontology.claim.payload import Payload
 from ontology.account.account import Account
-from ontology.claim.signature import SignatureInfo
+from ontology.crypto.digest import Digest
 from ontology.crypto.key_type import KeyType
 from ontology.exception.error_code import ErrorCode
 from ontology.exception.exception import SDKException
@@ -22,7 +22,7 @@ class Claim(object):
         self.__head = Header(kid)
         self.__payload = Payload(ver, iss.get_ont_id(), sub, int(time()), exp, jti, context, clm, clm_rev)
         self.__signature = ''
-        self.__merkle_proof = ''
+        self.__blockchain_proof = ''
 
     def signature(self, iss: Account, scheme: SignatureScheme = SignatureScheme.SHA256withECDSA):
         str_head = self.__head.to_json_str()
@@ -31,11 +31,40 @@ class Claim(object):
         handler = SignatureHandler(KeyType.from_signature_scheme(scheme), scheme)
         self.__signature = handler.generate_signature(iss.get_private_key_bytes(), msg)
 
-    def b64_signature_info(self):
+    def to_b64_signature(self):
         return base64.b64encode(self.__signature)
 
-    def generate_claim_str(self):
-        b64_head = self.__head.b64encode()
-        b64_payload = self.__payload.b64encode()
-        b64_signature = self.b64_signature_info()
-        return f'{b64_head}.{b64_payload}.{b64_signature}'
+    def to_bytes_signature(self):
+        return self.__signature.encode('utf-8')
+
+    def to_b64_blockchain_proof(self):
+        return base64.b64encode(self.__blockchain_proof)
+
+    def to_bytes_blockchain_proof(self):
+        return self.__blockchain_proof.encode('utf-8')
+
+    def get_claim_id(self):
+        claim_str = self.generate_b64_claim()
+        Digest.sha256(claim_str.encode('utf-8'))
+
+    @staticmethod
+    def generate_blockchain_proof(tx_hash: str, hex_contract_address: str, block_height: int, merkle_root: str,
+                                  proof: List[dict]):
+        if len(tx_hash) != 64:
+            raise SDKException(ErrorCode.other_error('Invalid TxHash.'))
+        if len(hex_contract_address) != 40:
+            raise SDKException(ErrorCode.other_error('Invalid contract address.'))
+        if not isinstance(block_height, int):
+            raise SDKException(ErrorCode.other_error('Invalid block height.'))
+        if len(merkle_root) != 64:
+            raise SDKException(ErrorCode.other_error('Invalid merkle root.'))
+        blockchain_proof = dict(Type='MerkleProof', TxnHash=tx_hash, ContractAddr=hex_contract_address,
+                                BlockHeight=block_height, MerkleRoot=merkle_root, Nodes=proof)
+        return blockchain_proof
+
+    def generate_b64_claim(self):
+        b64_head = self.__head.to_base64()
+        b64_payload = self.__payload.to_base64()
+        b64_signature = self.to_b64_signature()
+        b64_blockchain_proof = self.to_b64_blockchain_proof()
+        return f'{b64_head}.{b64_payload}.{b64_signature}.{b64_blockchain_proof}'
