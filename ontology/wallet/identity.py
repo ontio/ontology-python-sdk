6f276d8055d7bbe7a19a1b6acf9b3a6c751b496e
dingxin <wdx7266@vip.qq.com>
2019-01-03 15:00:41 +0800

    add property for controls and provide add_control interface

--- ontology/wallet/identity.py
+++ ontology/wallet/identity.py
@@ -14,11 +14,11 @@ class Identity(object):
         if controls is None:
             controls = list()
         if not isinstance(ont_id, str):
-            raise SDKException(ErrorCode.other_error('Invalid OntId.'))
+            raise SDKException(ErrorCode.invalid_ont_id_type)
         self.ont_id = ont_id
         self.label = label
         self.lock = lock
-        self.controls = controls
+        self.__controls = controls
         self.is_default = is_default
 
     def __iter__(self):
@@ -26,7 +26,25 @@ class Identity(object):
         data['ontid'] = self.ont_id
         data['label'] = self.label
         data['lock'] = self.lock
-        data['controls'] = self.controls
+        data['controls'] = self.__controls
         data['isDefault'] = self.is_default
         for key, value in data.items():
             yield (key, value)
+
+    @property
+    def controls(self):
+        return self.__controls
+
+    @controls.setter
+    def controls(self, ctrl_lst: List[Control]):
+        if not isinstance(ctrl_lst, list):
+            raise SDKException(ErrorCode.require_list_params)
+        for ctrl in ctrl_lst:
+            if not isinstance(ctrl, Control):
+                raise SDKException(ErrorCode.require_control_params)
+        self.__controls = ctrl_lst
+
+    def add_control(self, ctrl: Control):
+        if not isinstance(ctrl, Control):
+            raise SDKException(ErrorCode.params_type_error('control object is required.'))
+        self.__controls.append(ctrl)
