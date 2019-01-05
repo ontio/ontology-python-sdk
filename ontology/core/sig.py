f551cb973bfdbf78db4569c4cf820ccf78463fc3
dingxin <wdx7266@vip.qq.com>
2019-01-06 04:07:30 +0800

    add property

--- ontology/core/sig.py
+++ ontology/core/sig.py
@@ -13,22 +13,32 @@ from ontology.exception.exception import SDKException
 
 
 class Sig(object):
-    def __init__(self, public_keys=None, m: int = 0, sig_data=None):
+    def __init__(self, public_keys=None, m: int = 0, sig_data: list = None):
         self.public_keys = public_keys  # a list to save public keys
         self.m = m
-        self.sig_data = sig_data
+        if sig_data is None:
+            sig_data = list()
+        self.__sig_data = sig_data
 
     def __iter__(self):
         data = dict(M=self.m, publicKeys=list(), sigData=list())
         for key in self.public_keys:
             data['publicKeys'].append(binascii.b2a_hex(key).decode('ascii'))
-        for s_data in self.sig_data:
+        for s_data in self.__sig_data:
             data['sigData'].append(binascii.b2a_hex(s_data).decode('ascii'))
         for key, value in data.items():
             yield (key, value)
 
+    @property
+    def sig_data(self):
+        return self.__sig_data
+
+    @sig_data.setter
+    def sig_data(self, sig_data):
+        self.__sig_data = sig_data
+
     def serialize(self) -> bytes:
-        invoke_script = ProgramBuilder.program_from_params(self.sig_data)
+        invoke_script = ProgramBuilder.program_from_params(self.__sig_data)
         if len(self.public_keys) == 0:
             raise SDKException(ErrorCode.other_error('Public key in sig is empty.'))
 
@@ -57,7 +67,7 @@ class Sig(object):
         invocation_script = reader.read_var_bytes()
         verification_script = reader.read_var_bytes()
         sig = Sig()
-        sig.sig_data = ProgramBuilder.get_param_info(invocation_script)
+        sig.__sig_data = ProgramBuilder.get_param_info(invocation_script)
         info = ProgramBuilder.get_program_info(verification_script)
 
         sig.public_keys = info.pubkeys
