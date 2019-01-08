bead22b9e6d1cc6b48b46e65c653981ae6ce2272
dingxin <wdx7266@vip.qq.com>
2019-01-08 19:20:16 +0800

    update unit test

--- test/__init__.py
+++ test/__init__.py
@@ -8,6 +8,7 @@ from ontology.ont_sdk import OntologySdk
 password = environ['SDK_TEST_PASSWORD']
 sdk = OntologySdk()
 sdk.rpc.connect_to_test_net()
+sdk.restful.connect_to_test_net()
 wallet_path = path.join(path.dirname(__file__), 'test_wallet.json')
 wallet_manager = sdk.wallet_manager
 wallet_manager.open_wallet(wallet_path)
