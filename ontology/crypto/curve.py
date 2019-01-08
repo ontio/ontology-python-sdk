27110d44d6cbfcf50f1e845440acaa4c29775864
dingxin <wdx7266@vip.qq.com>
2019-01-09 01:22:28 +0800

    add from_str_curve

--- ontology/crypto/curve.py
+++ ontology/crypto/curve.py
@@ -4,6 +4,9 @@
 from enum import Enum, unique
 from cryptography.hazmat.primitives.asymmetric import ec
 
+from ontology.exception.error_code import ErrorCode
+from ontology.exception.exception import SDKException
+
 
 @unique
 class Curve(Enum):
@@ -23,3 +26,20 @@ class Curve(Enum):
             return Curve.P384.name
         elif Curve.P521.value == label:
             return Curve.P521.name
+        else:
+            raise SDKException(ErrorCode.unknown_asymmetric_key_type)
+
+    @staticmethod
+    def from_str_curve(str_curve: str):
+        if not isinstance(str_curve, str):
+            raise SDKException(ErrorCode.require_str_params)
+        if str_curve == 'P224':
+            return Curve.P224
+        elif str_curve == 'P256':
+            return Curve.P256
+        elif str_curve == 'P384':
+            return Curve.P384
+        elif str_curve == 'P521':
+            return Curve.P521
+        else:
+            raise SDKException(ErrorCode.unknown_asymmetric_key_type)
