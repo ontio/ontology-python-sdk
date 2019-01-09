1cc9effdbc6c15190d5556367be01e818102083d
dingxin <wdx7266@vip.qq.com>
2019-01-09 18:34:11 +0800

    update unit test

--- test/test_signature_handler.py
+++ test/test_signature_handler.py
@@ -3,33 +3,26 @@
 
 import unittest
 
-from ontology.account.account import Account
-from ontology.crypto.signature_handler import SignatureHandler
-
-from ontology.crypto.key_type import KeyType
 from ontology.crypto.signature_scheme import SignatureScheme
-from ontology.ont_sdk import OntologySdk
-
-acc = Account("75de8489fcb2dcaf2ef3cd607feffde18789de7da129b5e97c81e001793cb7cf")
-sdk = OntologySdk()
+from test import sdk, acct1, acct2
 
 
 class TestSignatureHandler(unittest.TestCase):
-    def test_signature_data(self):
-        sig = sdk.signature_data(acc, "sss".encode())
-        self.assertTrue(sdk.verify_signature(acc.get_public_key_bytes(), "sss".encode(), sig))
-
-    def testTx(self):
-        tx = sdk.native_vm.asset().new_transfer_transaction("ont",acc.get_address_base58(),acc.get_address_base58(),10,acc.get_address_base58(),20000,0)
-        tx.add_sign_transaction(acc)
-        self.assertTrue(sdk.verify_signature(tx.sigs[0].public_keys[0], tx.hash256_bytes(), tx.sigs[0].sig_data[0]))
+    def test_tx_signature(self):
+        b58_from_address = acct1.get_address_base58()
+        b58_to_address = acct2.get_address_base58()
+        tx = sdk.native_vm.asset().new_transfer_transaction('ont', b58_from_address, b58_to_address, 10,
+                                                            b58_from_address, 20000, 0)
+        tx.sign_transaction(acct1)
+        self.assertTrue(acct1.verify_signature(tx.hash256_bytes(), tx.sigs[0].sig_data[0]))
+        self.assertFalse(acct2.verify_signature(tx.hash256_bytes(), tx.sigs[0].sig_data[0]))
+        tx.add_sign_transaction(acct2)
+        self.assertTrue(acct2.verify_signature(tx.hash256_bytes(), tx.sigs[1].sig_data[0]))
 
     def test_generate_signature(self):
-        acc = Account("75de8489fcb2dcaf2ef3cd607feffde18789de7da129b5e97c81e001793cb7cf")
-        sigaa = "01cf157a48216bfcd455a97a39c0ad65bd1b27d1da07965b19848146045c9f2e5a12f905a5ee0923412d589e615a5d6954c58cade367dce67fcf13eaa82c12e87a"
-        byte_signature = acc.generate_signature("sss".encode(), SignatureScheme.SHA256withECDSA)
-
-        handler = SignatureHandler(KeyType.ECDSA, SignatureScheme.SHA256withECDSA)
-
-        res = handler.verify_signature(acc.get_public_key_bytes(), "sss".encode(), bytearray.fromhex(sigaa))
-        self.assertTrue(res)
+        msg = b'Attack!'
+        signature = acct1.generate_signature(msg)
+        result = acct1.verify_signature(msg, signature)
+        self.assertTrue(result)
+        result = acct2.verify_signature(msg, signature)
+        self.assertFalse(result)
