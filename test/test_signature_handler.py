98356ccee646eb8341c0303ba01f9e270194aed4
dingxin <wdx7266@vip.qq.com>
2019-01-11 18:28:15 +0800

    add unit test

--- test/test_signature_handler.py
+++ test/test_signature_handler.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import binascii
 import unittest
 
+from ontology.crypto.signature_scheme import SignatureScheme
 from test import sdk, acct1, acct2
+
+from ontology.crypto.key_type import KeyType
+from ontology.crypto.signature_handler import SignatureHandler
 
 
 class TestSignatureHandler(unittest.TestCase):
@@ -25,3 +30,12 @@ class TestSignatureHandler(unittest.TestCase):
         self.assertTrue(result)
         result = acct2.verify_signature(msg, signature)
         self.assertFalse(result)
+
+    def test_verify_cyano_signature(self):
+        msg = b'123'
+        sign = '010b6912568942a1e646b3a532dc904e965eb1085bab877bc34fe06768257f07b' \
+               '3079af3fa69fc759b51fa2bf894a7fd748ab5bc326c8663a01f90dcc518184e65'
+        pk = '03036c12be3726eb283d078dff481175e96224f0b0c632c7a37e10eb40fe6be889'
+        handler = SignatureHandler(KeyType.ECDSA, SignatureScheme.SHA256withECDSA)
+        result = handler.verify_signature(binascii.a2b_hex(pk), msg, binascii.a2b_hex(sign))
+        self.assertTrue(result)
