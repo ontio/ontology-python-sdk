ed190d5a5af2bdc94fee44bb3aa4bdbdf3f7e201
dingxin <wdx7266@vip.qq.com>
2019-01-14 16:07:22 +0800

    update interface

--- ontology/vm/params_builder.py
+++ ontology/vm/params_builder.py
@@ -74,7 +74,7 @@ class ParamsBuilder:
             raise SDKException(ErrorCode.param_err('type error, write byte failed.'))
 
     def to_bytes(self) -> bytes:
-        return a2b_hex(self.ms.to_bytes())
+        return a2b_hex(self.ms.hexlify())
 
     def to_bytearray(self) -> bytearray:
-        return bytearray(a2b_hex(self.ms.to_bytes()))
+        return bytearray(a2b_hex(self.ms.hexlify()))
