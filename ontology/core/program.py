0b588103f3ea2c636ac486fda993b188da3bc095
dingxin <wdx7266@vip.qq.com>
2019-01-14 16:20:57 +0800

    update transaction

--- ontology/core/program.py
+++ ontology/core/program.py
@@ -20,9 +20,9 @@ from ontology.common import define
 class ProgramBuilder(object):
 
     @staticmethod
-    def program_from_params(sigs):
+    def program_from_params(sig_list):
         code = bytearray()
-        for sig in sigs:
+        for sig in sig_list:
             code += ProgramBuilder.push_bytes(sig)
         return code
 
