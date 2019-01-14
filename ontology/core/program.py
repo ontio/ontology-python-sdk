176bce9c4817c1a3494b83935d309d14b306b0fe
dingxin <wdx7266@vip.qq.com>
2019-01-14 16:08:35 +0800

    update interface

--- ontology/core/program.py
+++ ontology/core/program.py
@@ -53,7 +53,7 @@ class ProgramBuilder(object):
             writer.write_uint32(len(data))
         writer.write_bytes(data)
         ms.flush()
-        res = ms.to_bytes()
+        res = ms.hexlify()
         StreamManager.release_stream(ms)
         res = bytes_reader(res)
         return res
