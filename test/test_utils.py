31452056aa017a7fdca931c7a12f5a38a33cf863
dingxin <wdx7266@vip.qq.com>
2019-01-22 18:50:56 +0800

    update unit test

--- test/test_utils.py
+++ test/test_utils.py
@@ -8,44 +8,16 @@ from ontology.utils import utils
 
 class TestUtil(unittest.TestCase):
     def test_get_random_bytes(self):
-        try:
-            length = -1
-            utils.get_random_bytes(length)
-        except ValueError:
-            raised = True
-            self.assertTrue(raised, 'Exception raised')
-        length = 0
-        self.assertEqual(len(utils.get_random_bytes(length)), length)
-        length = 1
-        self.assertEqual(len(utils.get_random_bytes(length)), length)
-        length = 64
-        self.assertEqual(len(utils.get_random_bytes(length)), length)
-        length = 256
-        self.assertEqual(len(utils.get_random_bytes(length)), length)
-        length = 1024
-        self.assertEqual(len(utils.get_random_bytes(length)), length)
-        length = 2048
-        self.assertEqual(len(utils.get_random_bytes(length)), length)
+        self.assertRaises(ValueError, utils.get_random_bytes, -1)
+        len_list = [0, 1, 64, 256, 1024, 2048]
+        for length in len_list:
+            self.assertEqual(len(utils.get_random_bytes(length)), length)
 
     def test_get_random_hex_str(self):
-        try:
-            length = -1
-            utils.get_random_hex_str(length)
-        except ValueError:
-            raised = True
-            self.assertTrue(raised, 'Exception raised')
-        length = 0
-        self.assertEqual(len(utils.get_random_hex_str(length)), length)
-        length = 1
-        self.assertEqual(len(utils.get_random_hex_str(length)), length)
-        length = 64
-        self.assertEqual(len(utils.get_random_hex_str(length)), length)
-        length = 256
-        self.assertEqual(len(utils.get_random_hex_str(length)), length)
-        length = 1024
-        self.assertEqual(len(utils.get_random_hex_str(length)), length)
-        length = 2048
-        self.assertEqual(len(utils.get_random_hex_str(length)), length)
+        self.assertRaises(ValueError, utils.get_random_hex_str, -1)
+        len_list = [0, 1, 64, 256, 1024, 2048]
+        for length in len_list:
+            self.assertEqual(len(utils.get_random_hex_str(length)), length)
 
 
 if __name__ == '__main__':
