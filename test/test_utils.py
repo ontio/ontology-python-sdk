c9d86b0283de2b5fec9181d95061db47cf913531
dingxin <wdx7266@vip.qq.com>
2019-01-22 18:47:46 +0800

    update unit test

--- test/test_utils.py
+++ test/test_utils.py
@@ -47,26 +47,6 @@ class TestUtil(unittest.TestCase):
         length = 2048
         self.assertEqual(len(utils.get_random_hex_str(length)), length)
 
-    def test_bigint_to_neo_bytes(self):
-        bs = utils.bigint_to_neo_bytes(-9175052165852779861)
-        bs1 = utils.bigint_to_neo_bytes(9175052165852779861)
-        bs2 = utils.bigint_to_neo_bytes(-9199634313818843819)
-        bs3 = utils.bigint_to_neo_bytes(9199634313818843819)
-        bs4 = utils.bigint_to_neo_bytes(-8380656)
-        bs5 = utils.bigint_to_neo_bytes(8380656)
-        bs6 = utils.bigint_to_neo_bytes(-8446192)
-        bs7 = utils.bigint_to_neo_bytes(8446192)
-        # TODO: bs8 = utils.bigint_to_neo_bytes(-0)
-        # TODO: bs9 = utils.bigint_to_neo_bytes(0)
-        self.assertEqual(bs.hex(), "abaaaaaaaaaaab80")
-        self.assertEqual(bs1.hex(), "555555555555547f")
-        self.assertEqual(bs2.hex(), "5555555555555480")
-        self.assertTrue(bs3.hex() == "abaaaaaaaaaaab7f")
-        self.assertTrue(bs4.hex() == "101f80")
-        self.assertTrue(bs5.hex() == "f0e07f")
-        self.assertTrue(bs6.hex() == "101f7fff")
-        self.assertTrue(bs7.hex() == "f0e08000")
-
 
 if __name__ == '__main__':
     unittest.main()
