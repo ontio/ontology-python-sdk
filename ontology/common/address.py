66f673a6684ac95f4f7858faf1d050ccebce043c
dingxin <wdx7266@vip.qq.com>
2019-02-01 17:57:58 +0800

    optimize address

--- ontology/common/address.py
+++ ontology/common/address.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import binascii
-from typing import List
-
 import base58
-from binascii import a2b_hex
+
+from typing import List
 
 from ontology.vm.op_code import CHECKSIG
 from ontology.crypto.digest import Digest
@@ -27,12 +25,12 @@ class Address(object):
 
     @staticmethod
     def to_script_hash(byte_script) -> bytes:
-        return a2b_hex(Digest.hash160(msg=byte_script, is_hex=True))
+        return Digest.hash160(msg=byte_script, is_hex=False)
 
     @staticmethod
     def address_from_bytes_pubkey(public_key: bytes):
         builder = ParamsBuilder()
-        builder.emit_push_byte_array(bytearray(public_key))
+        builder.emit_push_bytearray(bytearray(public_key))
         builder.emit(CHECKSIG)
         addr = Address(Address.to_script_hash(builder.to_bytes()))
         return addr
@@ -58,8 +56,8 @@ class Address(object):
     def b58encode(self):
         script_builder = Address.__COIN_VERSION + self.ZERO
         c256 = Digest.hash256(script_builder)[0:4]
-        out_byte_array = script_builder + bytearray(c256)
-        return base58.b58encode(bytes(out_byte_array)).decode('utf-8')
+        out_bytes = script_builder + c256
+        return base58.b58encode(out_bytes).decode('utf-8')
 
     def to_bytes(self):
         return self.ZERO
@@ -68,12 +66,12 @@ class Address(object):
         return bytearray(self.ZERO)
 
     def to_hex_str(self):
-        return binascii.b2a_hex(self.ZERO).decode('ascii')
+        return bytes.hex(self.ZERO)
 
     def to_reverse_hex_str(self):
         bytearray_zero = bytearray(self.ZERO)
         bytearray_zero.reverse()
-        return binascii.b2a_hex(bytearray_zero).decode('ascii')
+        return bytearray.hex(bytearray_zero)
 
     @staticmethod
     def b58decode(address: str):
