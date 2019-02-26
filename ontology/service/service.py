4cedf0d7cc49b2ff1b34f6f7db8d954393e86895
dingxin <wdx7266@vip.qq.com>
2019-02-26 13:48:11 +0800

    update service interface

--- ontology/service/service.py
+++ ontology/service/service.py
@@ -10,16 +10,27 @@ from ontology.service.sigsvr import SigSvr
 class Service(object):
     def __init__(self, sdk):
         self.__sdk = sdk
+        self.__claim = None
+        self.__sig_svr = None
+        self.__tx_verifier = None
+        self.__blockchain_proof = None
 
     def tx_verifier(self):
-        return TxVerifier(self.__sdk)
+        if self.__tx_verifier is None:
+            self.__tx_verifier = TxVerifier(self.__sdk)
+        return self.__tx_verifier
 
     def blockchain_proof(self):
-        return BlockchainProof(self.__sdk)
+        if self.__blockchain_proof is None:
+            self.__blockchain_proof = BlockchainProof(self.__sdk)
+        return self.__blockchain_proof
 
     def claim(self):
-        return Claim(self.__sdk)
+        if self.__claim is None:
+            self.__claim = Claim(self.__sdk)
+        return self.__claim
 
-    @staticmethod
-    def sig_svr():
-        return SigSvr()
+    def sig_svr(self):
+        if self.__sig_svr is None:
+            self.__sig_svr = SigSvr()
+        return self.__sig_svr
