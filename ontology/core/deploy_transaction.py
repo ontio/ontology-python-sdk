47394ec21fb08b12a7c11d87b5c2aea3d0d45246
dingxin <wdx7266@vip.qq.com>
2019-04-02 15:59:16 +0800

    change to private

--- ontology/core/deploy_transaction.py
+++ ontology/core/deploy_transaction.py
@@ -6,20 +6,20 @@ class DeployTransaction(Transaction):
     def __init__(self, code=None, need_storage=None, name=None, version=None, author=None, email=None,
                  description=None):
         super().__init__()
-        self.code = code
-        self.need_storage = need_storage
-        self.name = name
-        self.code_version = version
-        self.author = author
-        self.email = email
-        self.description = description
-        self.tx_type = TransactionType.DeployCode.value
+        self.__code = code
+        self.__need_storage = need_storage
+        self.__name = name
+        self.__code_version = version
+        self.__author = author
+        self.__email = email
+        self.__description = description
+        self.__tx_type = TransactionType.DeployCode.value
 
     def serialize_exclusive_data(self, writer: BinaryWriter):
-        writer.write_var_bytes(self.code)
-        writer.write_bool(self.need_storage)
-        writer.write_var_str(self.name)
-        writer.write_var_str(self.code_version)
-        writer.write_var_str(self.author)
-        writer.write_var_str(self.email)
-        writer.write_var_str(self.description)
+        writer.write_var_bytes(self.__code)
+        writer.write_bool(self.__need_storage)
+        writer.write_var_str(self.__name)
+        writer.write_var_str(self.__code_version)
+        writer.write_var_str(self.__author)
+        writer.write_var_str(self.__email)
+        writer.write_var_str(self.__description)
