948fbd621baf191bec143bf0d961d3f426fcf354
dingxin <wdx7266@vip.qq.com>
2019-05-06 11:41:38 +0800

    support asynchronous oep4

--- ontology/contract/neo/aio_oep4.py
+++ ontology/contract/neo/aio_oep4.py
@@ -17,10 +17,10 @@ along with The ontology.  If not, see <http://www.gnu.org/licenses/>.
 """
 from typing import Union
 
+from ontology.contract.neo.oep4 import Oep4
 from ontology.common.address import Address
-from ontology.utils.contract import Data, Event
-from ontology.contract.neo_vm import Oep4
 from ontology.account.account import Account
+from ontology.utils.contract import Data, Event
 from ontology.exception.exception import SDKException
 
 
@@ -41,7 +41,7 @@ class AioOep4(Oep4):
         This interface is used to get the symbol of the token asynchronously. E.g. “DX”.
         """
         tx = self.new_symbol_tx()
-        response = await self._sdk.default_network.send_raw_transaction_pre_exec(tx)
+        response = await self._sdk.default_aio_network.send_raw_transaction_pre_exec(tx)
         return Data.to_utf8_str(response.get('Result', ''))
 
     async def decimals(self):
@@ -65,9 +65,11 @@ class AioOep4(Oep4):
             total_supply = 0
         return total_supply
 
-    async def init(self, payer: Account, gas_price: int, gas_limit: int):
+    async def init(self, founder: Account, payer: Account, gas_price: int, gas_limit: int):
         tx = self.new_init_tx(payer.get_address(), gas_price, gas_limit)
-        tx.sign_transaction(payer)
+        tx.sign_transaction(founder)
+        if founder.get_address_bytes() != payer.get_address_bytes():
+            tx.add_sign_transaction(payer)
         tx_hash = await self._sdk.default_aio_network.send_raw_transaction(tx)
         return tx_hash
 
@@ -96,15 +98,16 @@ class AioOep4(Oep4):
         tx_hash = await self._sdk.default_aio_network.send_raw_transaction(tx)
         return tx_hash
 
-    async def transfer_multi(self, transfer_list: list, payer_acct: Account, signers: list, gas_limit: int,
-                             gas_price: int) -> str:
+    async def transfer_multi(self, transfer_list: list, signers: list, payer_acct: Account, gas_price: int,
+                             gas_limit: int) -> str:
         """
         This interface is used to transfer amount of token from from-account to to-account multiple times asynchronously.
         """
-        tx = self.new_transfer_multi_tx(transfer_list, payer_acct.get_address(), gas_limit, gas_price)
+        tx = self.new_transfer_multi_tx(transfer_list, payer_acct.get_address(), gas_price, gas_limit)
+        tx.sign_transaction(payer_acct)
         for signer in signers:
             tx.add_sign_transaction(signer)
-        tx_hash = await self._sdk.default_network.send_raw_transaction(tx)
+        tx_hash = await self._sdk.default_aio_network.send_raw_transaction(tx)
         return tx_hash
 
     async def approve(self, owner: Account, spender: Union[str, bytes, Address], amount: int, payer: Account,
@@ -129,3 +132,34 @@ class AioOep4(Oep4):
     async def query_multi_transfer_event(self, tx_hash: str) -> list:
         event = await self._sdk.get_network().get_contract_event_by_tx_hash(tx_hash)
         return self._parse_multi_transfer_event(event)
+
+    async def query_approve_event(self, tx_hash: str):
+        event = await self._sdk.default_aio_network.get_contract_event_by_tx_hash(tx_hash)
+        notify = Event.get_notify_by_contract_address(event, self._contract_address)
+        notify = Data.parse_addr_addr_int_notify(notify)
+        return notify
+
+    async def allowance(self, owner: Union[str, bytes, Address], spender: Union[str, bytes, Address]) -> int:
+        tx = self.new_allowance_tx(owner, spender)
+        response = await self._sdk.default_aio_network.send_raw_transaction_pre_exec(tx)
+        result = response.get('Result')
+        if len(result) == 0:
+            return 0
+        return int(result, base=16)
+
+    async def transfer_from(self, spender: Account, owner: Union[str, bytes, Address],
+                            to_address: Union[str, bytes, Address], value: int, payer: Account, gas_price: int,
+                            gas_limit: int) -> str:
+        tx = self.new_transfer_from_tx(spender.get_address(), owner, to_address, value, payer.get_address(), gas_price,
+                                       gas_limit)
+        tx.sign_transaction(spender)
+        if spender.get_address_bytes() != payer.get_address_bytes():
+            tx.add_sign_transaction(payer)
+        tx_hash = await self._sdk.default_aio_network.send_raw_transaction(tx)
+        return tx_hash
+
+    async def query_transfer_from_event(self, tx_hash: str):
+        event = await self._sdk.default_aio_network.get_contract_event_by_tx_hash(tx_hash)
+        notify = Event.get_notify_by_contract_address(event, self._contract_address)
+        notify = Data.parse_addr_addr_int_notify(notify)
+        return notify
--- test/test_aio_oep4.py
+++ test/test_aio_oep4.py
@@ -15,3 +15,185 @@ GNU Lesser General Public License for more details.
 You should have received a copy of the GNU Lesser General Public License
 along with The ontology.  If not, see <http://www.gnu.org/licenses/>.
 """
+import asyncio
+import unittest
+
+from Cryptodome.Random.random import randint
+
+from ontology.sdk import Ontology
+
+from test import sdk, acct1, acct2, acct3, acct4, not_panic_exception
+
+networks = [sdk.aio_rpc, sdk.aio_restful, sdk.websocket]
+
+contract_address = '1ddbb682743e9d9e2b71ff419e97a9358c5c4ee9'
+
+
+class TestOep4(unittest.TestCase):
+    def setUp(self) -> None:
+        sdk.default_network = sdk.rpc
+
+    @not_panic_exception
+    @Ontology.runner
+    async def test_query_name(self):
+        for network in networks:
+            sdk.default_aio_network = network
+            oep4 = sdk.neo_vm.aio_oep4(contract_address)
+            self.assertEqual('DXToken', await oep4.name())
+
+    @not_panic_exception
+    @Ontology.runner
+    async def test_get_symbol(self):
+        for network in networks:
+            sdk.default_network = network
+            oep4 = sdk.neo_vm.aio_oep4(contract_address)
+            self.assertEqual('DX', await oep4.symbol())
+
+    @not_panic_exception
+    @Ontology.runner
+    async def test_get_decimal(self):
+        contract_list = ['1ddbb682743e9d9e2b71ff419e97a9358c5c4ee9', '165b1227311d47c22cd073ef8f285d3bddc858ca',
+                         '8fecd2740b10a7410026774cc1f99fe14860873b']
+        decimal_list = [10, 32, 255]
+        for network in networks:
+            sdk.default_network = network
+            for index, address in enumerate(contract_list):
+                oep4 = sdk.neo_vm.aio_oep4(address)
+                self.assertEqual(decimal_list[index], await oep4.decimals())
+
+    @not_panic_exception
+    @Ontology.runner
+    async def test_init(self):
+        oep4 = sdk.neo_vm.aio_oep4()
+        oep4.hex_contract_address = contract_address
+        tx_hash = await oep4.init(acct1, acct2, 500, 20000000)
+        self.assertEqual(len(tx_hash), 64)
+        await asyncio.sleep(randint(16, 20))
+        notify = sdk.rpc.get_contract_event_by_tx_hash(tx_hash)['Notify'][0]
+        self.assertEqual('Already initialized!', bytes.fromhex(notify['States']).decode())
+
+    @not_panic_exception
+    @Ontology.runner
+    async def test_get_total_supply(self):
+        oep4 = sdk.neo_vm.aio_oep4()
+        oep4.hex_contract_address = contract_address
+        self.assertEqual(10000000000000000000, await oep4.total_supply())
+        try:
+            sdk.aio_rpc.connect_to_main_net()
+            oep4 = sdk.neo_vm.aio_oep4()
+            oep4.hex_contract_address = '6c80f3a5c183edee7693a038ca8c476fb0d6ac91'
+            self.assertEqual(10000000000, await oep4.total_supply())
+        finally:
+            sdk.aio_rpc.connect_to_test_net()
+
+    @not_panic_exception
+    @Ontology.runner
+    async def test_transfer(self):
+        oep4 = sdk.neo_vm.aio_oep4()
+        oep4.hex_contract_address = contract_address
+        from_acct = acct1
+        b58_to_address = acct2.get_address_base58()
+        value = 10
+        tx_hash = await oep4.transfer(from_acct, b58_to_address, value, from_acct, 500, 20000000)
+        self.assertEqual(64, len(tx_hash))
+        await asyncio.sleep(randint(14, 20))
+        notify = await oep4.query_transfer_event(tx_hash)
+        self.assertEqual('transfer', notify['States'][0])
+        self.assertEqual(from_acct.get_address_base58(), notify['States'][1])
+        self.assertEqual(b58_to_address, notify['States'][2])
+        self.assertEqual(value, notify['States'][3])
+
+    @not_panic_exception
+    @Ontology.runner
+    async def test_balance_of(self):
+        oep4 = sdk.neo_vm.aio_oep4()
+        oep4.hex_contract_address = contract_address
+        b58_address1 = acct3.get_address_base58()
+        b58_address2 = acct4.get_address_base58()
+        balance = await oep4.balance_of(b58_address1)
+        self.assertGreaterEqual(balance, 10)
+        balance = await oep4.balance_of(b58_address2)
+        self.assertGreaterEqual(balance, 1)
+
+    @not_panic_exception
+    @Ontology.runner
+    async def test_transfer_multi(self):
+        oep4 = sdk.neo_vm.aio_oep4()
+        oep4.hex_contract_address = contract_address
+        transfer_list = list()
+
+        b58_from_address1 = acct1.get_address_base58()
+        b58_from_address2 = acct2.get_address_base58()
+        from_address_list = [b58_from_address1, b58_from_address2]
+
+        b58_to_address1 = acct2.get_address_base58()
+        b58_to_address2 = acct3.get_address_base58()
+        to_address_list = [b58_to_address1, b58_to_address2]
+
+        value_list = [1, 2]
+
+        transfer1 = [b58_from_address1, b58_to_address1, value_list[0]]
+        transfer2 = [b58_from_address2, b58_to_address2, value_list[1]]
+
+        signers = [acct1, acct2, acct3]
+        transfer_list.append(transfer1)
+        transfer_list.append(transfer2)
+
+        tx_hash = await oep4.transfer_multi(transfer_list, signers, acct1, 500, 20000000)
+        self.assertEqual(64, len(tx_hash))
+        await asyncio.sleep(randint(14, 20))
+        notify_list = await oep4.query_multi_transfer_event(tx_hash)
+        self.assertEqual(len(transfer_list), len(notify_list))
+        for index, notify in enumerate(notify_list):
+            self.assertEqual('transfer', notify['States'][0])
+            self.assertEqual(from_address_list[index], notify['States'][1])
+            self.assertEqual(to_address_list[index], notify['States'][2])
+            self.assertEqual(value_list[index], notify['States'][3])
+
+    @not_panic_exception
+    @Ontology.runner
+    async def test_approve(self):
+        oep4 = sdk.neo_vm.aio_oep4()
+        oep4.hex_contract_address = contract_address
+        b58_spender_address = acct2.get_address_base58()
+        amount = 10
+        tx_hash = await oep4.approve(acct1, b58_spender_address, amount, acct1, 500, 20000000)
+        self.assertEqual(len(tx_hash), 64)
+        await asyncio.sleep(randint(14, 20))
+        event = await oep4.query_approve_event(tx_hash)
+        self.assertEqual(contract_address, event.get('ContractAddress', ''))
+        states = event['States']
+        self.assertEqual('approval', states[0])
+        self.assertEqual(acct1.get_address_base58(), states[1])
+        self.assertEqual(b58_spender_address, states[2])
+        self.assertEqual(amount, states[3])
+
+    @not_panic_exception
+    @Ontology.runner
+    async def test_allowance(self):
+        oep4 = sdk.neo_vm.aio_oep4()
+        oep4.hex_contract_address = contract_address
+        allowance = await oep4.allowance(acct1.get_address(), acct2.get_address())
+        self.assertGreaterEqual(allowance, 1)
+
+    @not_panic_exception
+    @Ontology.runner
+    async def test_transfer_from(self):
+        oep4 = sdk.neo_vm.aio_oep4()
+        oep4.hex_contract_address = contract_address
+        b58_from_address = acct1.get_address_base58()
+        b58_to_address = acct3.get_address_base58()
+        value = 1
+        tx_hash = await oep4.transfer_from(acct2, b58_from_address, b58_to_address, value, acct1, 500, 20000000)
+        self.assertEqual(64, len(tx_hash))
+        await asyncio.sleep(randint(14, 20))
+        event = await oep4.query_transfer_from_event(tx_hash)
+        self.assertEqual(contract_address, event.get('ContractAddress', ''))
+        self.assertEqual('transfer', event['States'][0])
+        self.assertEqual(b58_from_address, event['States'][1])
+        self.assertEqual(b58_to_address, event['States'][2])
+        self.assertEqual(value, event['States'][3])
+
+
+if __name__ == '__main__':
+    unittest.main()
