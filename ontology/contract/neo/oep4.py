9e2180a1eb3e33188891a0c59019bd0b14e7c0c9
dingxin <wdx7266@vip.qq.com>
2019-05-06 11:17:32 +0800

    refactor oep4 & unit test

--- ontology/contract/neo/oep4.py
+++ ontology/contract/neo/oep4.py
@@ -130,7 +130,7 @@ class Oep4(object):
         tx_hash = self._sdk.default_network.send_raw_transaction(tx)
         return tx_hash
 
-    def new_balance_of_tx(self, owner: Union[str, Address]) -> InvokeTransaction:
+    def new_balance_of_tx(self, owner: Union[str, bytes, Address]) -> InvokeTransaction:
         """
         This interface is used to generate transaction which can get the account balance of another account with owner address.
         """
@@ -188,9 +188,7 @@ class Oep4(object):
                 raise SDKException(ErrorCode.param_err('the data type of value should be int.'))
             if item[2] < 0:
                 raise SDKException(ErrorCode.param_err('the value should be equal or great than 0.'))
-            from_address_array = Address.b58decode(item[0]).to_bytes()
-            to_address_array = Address.b58decode(item[1]).to_bytes()
-            transfer_list[index] = [from_address_array, to_address_array, item[2]]
+            transfer_list[index] = [Address.b58decode(item[0]), Address.b58decode(item[1]), item[2]]
         for item in transfer_list:
             func.add_params_value(item)
         params = InvokeTransaction.generate_invoke_code(self._contract_address, func)
@@ -220,7 +218,7 @@ class Oep4(object):
         if amount < 0:
             raise SDKException(ErrorCode.param_err('the amount should be equal or great than 0.'))
         func = InvokeFunction('approve')
-        func.set_params_value(owner, spender, amount)
+        func.set_params_value(Address.b58decode(owner), Address.b58decode(spender), amount)
         params = InvokeTransaction.generate_invoke_code(self._contract_address, func)
         tx = InvokeTransaction(payer, gas_price, gas_limit, params)
         return tx
@@ -252,11 +250,11 @@ class Oep4(object):
         return notify_list
 
     def query_multi_transfer_event(self, tx_hash: str) -> list:
-        event = self._sdk.get_network().get_contract_event_by_tx_hash(tx_hash)
+        event = self._sdk.default_network.get_contract_event_by_tx_hash(tx_hash)
         return self._parse_multi_transfer_event(event)
 
     def query_approve_event(self, tx_hash: str):
-        event = self._sdk.get_network().get_contract_event_by_tx_hash(tx_hash)
+        event = self._sdk.default_network.get_contract_event_by_tx_hash(tx_hash)
         notify = Event.get_notify_by_contract_address(event, self._contract_address)
         notify = Data.parse_addr_addr_int_notify(notify)
         return notify
@@ -264,7 +262,7 @@ class Oep4(object):
     def new_allowance_tx(self, owner: Union[str, bytes, Address],
                          spender: Union[str, bytes, Address]) -> InvokeTransaction:
         func = InvokeFunction('allowance')
-        func.set_params_value(owner, spender)
+        func.set_params_value(Address.b58decode(owner), Address.b58decode(spender))
         tx = InvokeTransaction()
         tx.add_invoke_code(self._contract_address, func)
         return tx
@@ -275,7 +273,7 @@ class Oep4(object):
         result = response.get('Result')
         if len(result) == 0:
             return 0
-        return int(result)
+        return int(result, base=16)
 
     def new_transfer_from_tx(self, spender: Union[str, bytes, Address], owner: Union[str, bytes, Address],
                              to_address: Union[str, bytes, Address], value: int, payer: Union[str, bytes, Address],
@@ -283,7 +281,8 @@ class Oep4(object):
         func = InvokeFunction('transferFrom')
         if not isinstance(value, int):
             raise SDKException(ErrorCode.param_err('the data type of value should be int.'))
-        func.set_params_value(spender, owner, to_address, value)
+        func.set_params_value(Address.b58decode(spender), Address.b58decode(owner), Address.b58decode(to_address),
+                              value)
         tx = InvokeTransaction(payer, gas_price, gas_limit)
         tx.add_invoke_code(self._contract_address, func)
         return tx
@@ -294,6 +293,12 @@ class Oep4(object):
                                        gas_limit)
         tx.sign_transaction(spender)
         if spender.get_address_bytes() != payer.get_address_bytes():
-            tx.add_sign_transaction(spender)
+            tx.add_sign_transaction(payer)
         tx_hash = self._sdk.default_network.send_raw_transaction(tx)
         return tx_hash
+
+    def query_transfer_from_event(self, tx_hash: str):
+        event = self._sdk.default_network.get_contract_event_by_tx_hash(tx_hash)
+        notify = Event.get_notify_by_contract_address(event, self._contract_address)
+        notify = Data.parse_addr_addr_int_notify(notify)
+        return notify
