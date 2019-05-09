8d46aae76bee97caae2f691dba875a6dad080124
dingxin <wdx7266@vip.qq.com>
2019-05-09 13:32:19 +0800

    add UT for from_hd_pubkey

--- test/test_address.py
+++ test/test_address.py
@@ -21,6 +21,7 @@ along with The ontology.  If not, see <http://www.gnu.org/licenses/>.
 
 import unittest
 
+from ontology.crypto.hd_public_key import HDPublicKey
 from test import sdk
 
 from ontology.utils import utils
@@ -28,6 +29,10 @@ from ontology.common.address import Address
 
 
 class TestAddress(unittest.TestCase):
+    def setUp(self):
+        self.bip32_pubkey = b'xpub6CbjoChWbA9TdLQKghCH5GzRrvjPxTiz6kYkW3frXyyN6vfbR7wGkYqd9jyEqkpYRe33oe5sQbamndiWQjc' \
+                            b'9X3mr29HdKWqgjwb6G3xYXFo'
+
     def test_address_from_vm_code(self):
         code = '55c56b6a00527ac46a51527ac46a00c30548656c6c6f7d9c7c756419006a51c300c36a52527ac46a52c3650d006c7' \
                '566620300006c756652c56b6a00527ac46a00c3681553797374656d2e52756e74696d652e4e6f74696679516c7566'
@@ -43,6 +48,20 @@ class TestAddress(unittest.TestCase):
         self.assertEqual(rand_code, zero)
         decode_address = Address.b58decode(b58_address).to_bytes()
         self.assertEqual(rand_code, decode_address)
+
+    def test_from_hd_pubkey(self):
+        hd_pub_key = HDPublicKey.b58decode(self.bip32_pubkey)
+        address = Address.from_hd_public_key(hd_pub_key)
+        self.assertEqual('AW8Tf5R4kyURy6LQ8Th181Z5GpTovWGLg6', address.b58encode())
+        child_address = ['ARXRQog4iZazp5YfXRyDZvU6ahrt3c2bb7', 'APXh8MqcARUgafqvUNnpECzwKDtipkf3Zr',
+                         'ASpmd1MpFSpQ5rhicjRDqBpE1inP3Z7tus', 'APA3M4BRqjBsHXRkeTFiFVb4X1u8FiEgAr',
+                         'AKq5SBTCzHBaqWWDUTGvekbsNJKKtf4ff5', 'APzMnHqrGF1cGZyAdFCwEL29TnAgCuBrY6',
+                         'AJ8LEkLGeNsWvVrP7SgK9szoZ1MGgUTq1s', 'AJo49LSK6rQEwc6qTYMsAZmvLPRrb6qcWa',
+                         'AYzCcNY3PwV432iXVREpDHvpX166KN45xP', 'AbL1wvGCnbzywHBuX1VwQev8xuhJxbPE4P']
+        for i in range(10):
+            child_pks = HDPublicKey.from_path(hd_pub_key, '{change}/{index}'.format(change=0, index=i))
+            address = Address.from_hd_public_key(child_pks[-1])
+            self.assertEqual(child_address[i], address.b58encode())
 
 
 if __name__ == '__main__':
