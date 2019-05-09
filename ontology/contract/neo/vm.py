4529ee10a8fbd7f50357fad53c6ea65bb751083c
dingxin <wdx7266@vip.qq.com>
2019-05-09 14:52:18 +0800

    add type hint

--- ontology/contract/neo/vm.py
+++ ontology/contract/neo/vm.py
@@ -21,9 +21,9 @@ from typing import Union
 from ontology.common.address import Address
 from ontology.contract.neo.oep4 import Oep4
 from ontology.contract.neo.aio_oep4 import AioOep4
+from ontology.contract.neo.claim_record import ClaimRecord
 from ontology.core.deploy_transaction import DeployTransaction
 from ontology.core.invoke_transaction import InvokeTransaction
-from ontology.contract.neo.claim_record import ClaimRecord
 from ontology.contract.neo.abi.abi_function import AbiFunction
 from ontology.contract.neo.invoke_function import InvokeFunction
 
@@ -32,13 +32,13 @@ class NeoVm(object):
     def __init__(self, sdk):
         self.__sdk = sdk
 
-    def oep4(self, hex_contract_address: str = ''):
+    def oep4(self, hex_contract_address: str = '') -> Oep4:
         return Oep4(hex_contract_address, self.__sdk)
 
-    def aio_oep4(self, hex_contract_address: str = ''):
+    def aio_oep4(self, hex_contract_address: str = '') -> AioOep4:
         return AioOep4(hex_contract_address, self.__sdk)
 
-    def claim_record(self):
+    def claim_record(self) -> ClaimRecord:
         return ClaimRecord(self.__sdk)
 
     @staticmethod
@@ -46,8 +46,15 @@ class NeoVm(object):
         return Address.from_avm_code(avm_code)
 
     @staticmethod
-    def make_deploy_transaction(code: str, need_storage: bool, name: str, code_version: str, author: str,
-                                email: str, description: str, gas_price: int, gas_limit: int,
+    def make_deploy_transaction(code: str,
+                                need_storage: bool,
+                                name: str,
+                                code_version: str,
+                                author: str,
+                                email: str,
+                                description: str,
+                                gas_price: int,
+                                gas_limit: int,
                                 b58_payer_address: str) -> DeployTransaction:
         tx = DeployTransaction(code, need_storage, name, code_version, author, email, description, gas_price, gas_limit,
                                b58_payer_address)
@@ -55,8 +62,10 @@ class NeoVm(object):
 
     @staticmethod
     def make_invoke_transaction(contract_address: Union[str, bytes, bytearray, Address],
-                                func: Union[AbiFunction, InvokeFunction], payer: Union[str, bytes, Address] = b'',
-                                gas_price: int = 0, gas_limit: int = 0):
+                                func: Union[AbiFunction, InvokeFunction],
+                                payer: Union[str, bytes, Address] = b'',
+                                gas_price: int = 0,
+                                gas_limit: int = 0) -> InvokeTransaction:
         tx = InvokeTransaction(payer, gas_price, gas_limit)
         tx.add_invoke_code(contract_address, func)
         return tx
